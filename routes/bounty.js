3bfdd092d4c7573f7070a5c139293ebcbedfb185
Tyler Parker <tparker42d2@gmail.com>
2021-09-20 15:43:57 -0600

    fixed imports in App

--- routes/bounty.js
+++ routes/bounty.js
@@ -2,14 +2,6 @@ const express = require('express')
 const bountyRouter = express.Router()
 const Bounty = require('../models/bounty')
 
-// const bounties = [
-//     {firstName: 'Han', lastName: 'Soloooooooo', living: "Dead", reward: 20000, type: 'Smuggler', imgURL: 'https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/4/1433405419545/90fa370a-d9c4-451a-93ae-de3265bef916-1020x612.jpeg?width=300&quality=45&auto=format&fit=max&dpr=2&s=4929a6f3bae46b42a6cf581cf8f9bc84', _id: uuid()},
-//     {firstName: 'Lando', lastName: 'Calrissian', living: "Alive", reward: 10000, type: 'Traitor', imgURL: 'https://www.denofgeek.com/wp-content/uploads/2017/12/star-wars-lando-calrissian.jpg?resize=768%2C432', _id: uuid()},
-//     {firstName: 'Bo-Katan', lastName: 'Kryze', living: "Alive", reward: 80000, type: 'Bounty hunter', imgURL: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Bo-katan_Kryze.jpg/250px-Bo-katan_Kryze.jpg', _id: uuid()},
-//     {firstName: 'Jyn', lastName: 'Ersooooooo', living: "Dead", reward: 100000, type: 'Fugitive', imgURL: 'https://www.syfy.com/sites/syfy/files/styles/1200x680_hero/public/wire/legacy/felicity-jones-rogue-one.jpg', _id: uuid()},
-//     {firstName: 'Poe', lastName: 'Dameron', living: "Alive", reward: 500000, type: 'Fugitive', imgURL: 'https://upload.wikimedia.org/wikipedia/en/0/0b/Poe_Dameron-Force_Awakens_%282015%29.png', _id: uuid()}
-// ]
-
 //Get all bounties
 bountyRouter.get('/', (req, res, next) => {
     Bounty.find((err, bounties) => {
